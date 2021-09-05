b9b241b0d5468d69d9a0a3d2d2454c9053ee156e
nefertirii <nefertiri972@gmail.com>
2021-09-06 02:07:21 +0900

    Add test code
    - @ExtendWith
    - @WebMvcTest

---
+++ src/test/java/com/spring/board/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.spring.board.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    void lombok() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
