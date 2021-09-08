863113947a366562d5ee322cdcc5e18317fd489d
nefertirii <nefertiri972@gmail.com>
2021-09-09 00:51:42 +0900

    Add domain object
    - Posts 엔티티 클래스 추가
    - Posts 리포지토리 추가
    - Posts 리포지토리 테스트 추가

---
+++ src/test/java/com/spring/board/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.spring.board.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void savePosts() {
+        // given
+        String title = "title";
+        String content = "content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
