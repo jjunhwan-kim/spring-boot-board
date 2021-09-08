1cc62567f600b9f52591265f0cb497268422edb6
nefertirii <nefertiri972@gmail.com>
2021-09-09 00:54:40 +0900

    Add posts save api
    - API 컨트롤러 추가
    - Request DTO 추가
    - PostsSerivce 클래스 추가
      - 트랜잭션, 도메인 기능 간의 순서 보장
    - API 테스트 추가

---
+++ src/main/java/com/spring/board/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.spring.board.service.posts;
+
+import com.spring.board.domain.posts.PostsRepository;
+import com.spring.board.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
