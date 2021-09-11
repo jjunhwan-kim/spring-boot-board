d3d2bde82b9229fbb990782048e7e4068b4ff6c4
nefertirii <nefertiri972@gmail.com>
2021-09-11 21:45:53 +0900

    Add post update and delete page
    - 게시물 수정, 삭제 화면 추가

--- src/main/java/com/spring/board/web/IndexController.java
+++ src/main/java/com/spring/board/web/IndexController.java
@@ -3,6 +3,7 @@ package com.spring.board.web;
 import com.spring.board.service.posts.PostsService;
 import com.spring.board.web.dto.PageListDto;
 import com.spring.board.web.dto.PostsListResponseDto;
+import com.spring.board.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,5 +66,21 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String postsView(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-view";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
